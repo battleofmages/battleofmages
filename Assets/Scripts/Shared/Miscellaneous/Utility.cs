5143eae771b8537d972e2957a2dbc8037fe28533
Eduard Urbach <e.urbach@gmail.com>
2015-03-21 14:41:46 +0900

    Get build date automatically
    https://www.pivotaltracker.com/story/show/90828570

---
+++ Assets/Scripts/Shared/Miscellaneous/Utility.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public static class Utility {
+	// GetBuildDate
+	public static DateTime GetBuildDate() {
+		string filePath = System.Reflection.Assembly.GetCallingAssembly().Location;
+
+		const int c_PeHeaderOffset = 60;
+		const int c_LinkerTimestampOffset = 8;
+
+		byte[] b = new byte[2048];
+		System.IO.Stream s = null;
+		
+		try {
+			s = new System.IO.FileStream(filePath, System.IO.FileMode.Open, System.IO.FileAccess.Read);
+			s.Read(b, 0, 2048);
+		} finally {
+			if(s != null) {
+				s.Close();
+			}
+		}
+		
+		int i = System.BitConverter.ToInt32(b, c_PeHeaderOffset);
+		int secondsSince1970 = System.BitConverter.ToInt32(b, i + c_LinkerTimestampOffset);
+
+		DateTime dt = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+		dt = dt.AddSeconds(secondsSince1970);
+		dt = dt.ToLocalTime();
+
+		return dt;
+	}
+}
---
+++ Assets/Scripts/UI/Lobby/BuildVersion.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Globalization;
+
+public class BuildVersion : MonoBehaviour {
+	public Text textComponent;
+
+	// Start
+	void Start() {
+		var date = Utility.GetBuildDate();
+		var month = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(date.Month);
+		var day = date.Day.ToString();
+		var year = date.Year.ToString();
+
+		textComponent.text = textComponent.text.Replace("{month}", month);
+		textComponent.text = textComponent.text.Replace("{day}", day);
+		textComponent.text = textComponent.text.Replace("{year}", year);
+	}
+}
