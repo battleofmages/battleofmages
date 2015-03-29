d1a91a02aea7b1b2441b999e38d043ffa2200ab3
Mathias Danielsen <mathais234@gmail.com>
2015-03-30 01:00:27 +0200

    Started work on TraitsWidget

---
+++ Assets/Scripts/UI/Lobby/Traits/TraitsWidget.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using BoM.UI.Lobby;
+
+public class TraitsWidget : MonoBehaviour, Initializable {
+	// Init
+	public void Init() {
+		Login.instance.onLogIn += account => {
+			account.traits.Connect(
+				this,
+				data => { LogManager.General.Log("Traits: " + data.ToString());
+			}
+			);
+		};
+	}
+}
