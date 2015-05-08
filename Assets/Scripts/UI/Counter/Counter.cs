a073cdfd10532782ca858beeca661b4a91a1f27e
Eduard Urbach <e.urbach@gmail.com>
2015-05-08 18:02:13 +0900

    Added FPS counter

---
+++ Assets/Scripts/UI/Counter/Counter.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour {
+	public Text textComponent;
+	protected int counter = 0;
+	private string format;
+	private Color defaultColor;
+	
+	[System.Serializable]
+	public struct LimitColor {
+		public int limit;
+		public Color color;
+	}
+	
+	public LimitColor[] colors;
+	
+	// Start
+	void Start() {
+		format = textComponent.text;
+		defaultColor = textComponent.color;
+		
+		InvokeRepeating("UpdateCounter", 0.1f, 1.0f);
+	}
+	
+	// UpdateCounter
+	void UpdateCounter() {
+		// Text
+		textComponent.text = format.Replace("{fps}", counter.ToString());
+		
+		// Color
+		UpdateColor();
+		
+		// Reset counter
+		counter = 0;
+	}
+	
+	// UpdateColor
+	void UpdateColor() {
+		foreach(var limit in colors) {
+			if(counter >= limit.limit)
+				continue;
+			
+			textComponent.color = limit.color;
+			return;
+		}
+		
+		textComponent.color = defaultColor;
+	}
+}
---
+++ Assets/Scripts/UI/Counter/FPSCounter.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+// FPSCounter
+// Fixed update rate of 1 second, it doesn't support dynamic update rates.
+public class FPSCounter : Counter {
+	// Update
+	void Update() {
+		counter += 1;
+	}
+}
