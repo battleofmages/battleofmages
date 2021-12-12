2d8b0ff06d7b47f929f266d5acbf1f032c591cb3
Eduard Urbach <e.urbach@gmail.com>
2021-12-13 01:01:27 +0900

    Improved Rotate script
    Former-commit-id: dae6b340db6323761b5e12b345830bec9cc72e75

---
+++ Assets/Items/Rotate.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+	public Vector3 axis;
+	public float speed;
+
+	private void Update() {
+		transform.Rotate(axis, speed * Time.deltaTime);
+	}
+}
--- Assets/Items/RotateY.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class RotateY : MonoBehaviour {
-	public float rotationSpeed;
-
-	private void Update() {
-		transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
-	}
-}
