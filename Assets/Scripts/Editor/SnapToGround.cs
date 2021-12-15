7bb1940a0e52dfa602cd2ff4fd74a858a1ddc3b5
Eduard Urbach <e.urbach@gmail.com>
2021-12-15 16:52:52 +0900

    Improved post-process effects
    Former-commit-id: d176e4107694a64c1acfd7472a480e2b9e19d159

---
+++ Assets/Scripts/Editor/SnapToGround.cs
@@ -0,0 +1,32 @@
+using UnityEditor;
+using UnityEngine;
+
+public class SnapToGround : MonoBehaviour {
+	[MenuItem("Custom/Snap To Ground %g")]
+	public static void SnapGround() {
+		foreach(var transform in Selection.transforms) {
+			var hits = Physics.RaycastAll(transform.position + Vector3.up, Vector3.down);
+			var highest = -Mathf.Infinity;
+			var count = 0;
+
+			foreach(var hit in hits) {
+				if(hit.collider.gameObject == transform.gameObject) {
+					continue;
+				}
+
+				if(hit.point.y > highest) {
+					highest = hit.point.y;
+				}
+
+				count++;
+			}
+
+			if(count == 0) {
+				return;
+			}
+
+			Undo.RecordObject(transform, "Snap to ground");
+			transform.position = new Vector3(transform.position.x, highest, transform.position.z);
+		}
+	}
+}
