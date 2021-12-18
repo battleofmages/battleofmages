ee323857dce5c1458270304cc4f2ab07271d8681
Eduard Urbach <e.urbach@gmail.com>
2021-12-19 01:27:56 +0900

    Improved pooling
    Former-commit-id: f49d0beeed53310e0c028885c4de3bf5adf2e7bc

--- Assets/Scripts/Skill/Manager.cs
+++ Assets/Scripts/Skill/Manager.cs
@@ -10,17 +10,7 @@ namespace BoM.Skill {
 			foreach(var element in elements) {
 				foreach(var skill in element.skills) {
 					skill.element = element;
-					skill.pool = new ObjectPool<Instance>(() => {
-						return GameObject.Instantiate(skill.prefab);
-					}, instance => {
-						instance.gameObject.SetActive(true);
-					}, instance => {
-						instance.gameObject.SetActive(false);
-					}, instance => {
-						Destroy(instance.gameObject);
-					}, false, 32, 64);
-
-					PoolManager.pools.Add(skill.prefab, skill.pool);
+					skill.pool = PoolManager.GetPool(skill.prefab);
 				}
 			}
 		}
