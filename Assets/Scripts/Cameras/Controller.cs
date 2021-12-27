b42887c9c66974112ca9e1d591efc4843597e996
Eduard Urbach <e.urbach@gmail.com>
2021-12-28 02:52:22 +0900

    Added camera frustum checks
    Former-commit-id: 5572481df75b7335fedb94a228b2d1bc25f980dc

--- Assets/Scripts/Cameras/Controller.cs
+++ Assets/Scripts/Cameras/Controller.cs
@@ -4,21 +4,27 @@ using UnityEngine.Rendering.Universal;
 
 namespace BoM.Cameras {
 	public class Controller : MonoBehaviour {
+		public Camera cam;
 		public Transform target;
 		public Transform center;
 		public LayerMask layerMask;
 		public VolumeProfile volumeProfile;
 		public float distanceAdjustmentSpeed;
+		public float distanceToObstacle;
 		private float maxDistance;
 		private float targetDistance;
 		private float distance;
 		private RaycastHit hit;
 		private DepthOfField depthOfField;
+		private Vector3[] frustumCorners;
+		private Rect viewport;
 
 		private void Awake() {
 			maxDistance = -target.localPosition.z;
 			distance = maxDistance;
 			targetDistance = maxDistance;
+			frustumCorners = new Vector3[4];
+			viewport = new Rect(0, 0, 1, 1);
 
 			if(!volumeProfile.TryGet(out depthOfField)) {
 				throw new System.NullReferenceException(nameof(depthOfField));
@@ -26,10 +32,21 @@ namespace BoM.Cameras {
 		}
 
 		private void Update() {
-			if(Physics.Raycast(center.position, -center.forward, out hit, maxDistance, layerMask)) {
-				targetDistance = (hit.point - center.position).magnitude;
-			} else {
-				targetDistance = maxDistance;
+			cam.CalculateFrustumCorners(viewport, cam.nearClipPlane, Camera.MonoOrStereoscopicEye.Mono, frustumCorners);
+			targetDistance = maxDistance;
+
+			for(int i = 0; i < 4; i++) {
+				var cornerPosition = cam.transform.TransformVector(frustumCorners[i]);
+
+				if(!Physics.Raycast(center.position, cam.transform.position - center.position + cornerPosition, out hit, maxDistance + distanceToObstacle, layerMask)) {
+					continue;
+				}
+
+				var cornerDistance = (hit.point - center.position).magnitude - distanceToObstacle;
+
+				if(cornerDistance < targetDistance) {
+					targetDistance = cornerDistance;
+				}
 			}
 
 			distance = Mathf.Lerp(distance, targetDistance, Time.deltaTime * distanceAdjustmentSpeed);
