69197c5703a4d302c138b43990cd02335057ba35
Eduard Urbach <e.urbach@gmail.com>
2022-01-03 15:47:31 +0900

    Improved skill bar
    Former-commit-id: 3bdadc55e6a2e3fa99d2b62f85535bbe858cdeb8

--- Assets/Scripts/UI/SkillSlot.cs
+++ Assets/Scripts/UI/SkillSlot.cs
@@ -1,4 +1,5 @@
 using BoM.Core;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,6 +8,7 @@ namespace BoM.UI {
 	public class SkillSlotData : MonoBehaviour {
 		[SerializeField] protected Image image;
 		[SerializeField] protected Button button;
+		[SerializeField] protected TextMeshProUGUI coolDownLabel;
 		protected ISkillSlot slot;
 	}
 
@@ -20,11 +22,15 @@ namespace BoM.UI {
 				if(slot == null || slot.Skill == null) {
 					button.interactable = false;
 				} else {
-					image.color = Color.white;
 					image.sprite = slot.Skill.Icon;
 					button.interactable = true;
 				}
 			}
+		}
+
+		private void Awake() {
+			button.interactable = false;
+			coolDownLabel.enabled = false;
 		}
 
 		private void Update() {
@@ -32,7 +38,35 @@ namespace BoM.UI {
 				return;
 			}
 
-			button.interactable = slot.IsReady;
+			var isReady = slot.IsReady;
+			var progress = Mathf.Clamp((Time.time - slot.LastUsed) / slot.Skill.CoolDown, 0f, 1f);
+
+			if(isReady) {
+				if(button.interactable == false) {
+					button.interactable = true;
+					image.fillAmount = 1f;
+					image.color = Color.white;
+					coolDownLabel.enabled = false;
+				}
+
+				return;
+			} else {
+				if(button.interactable == true) {
+					button.interactable = false;
+
+					if(slot.Skill.CoolDown >= 1f) {
+						coolDownLabel.enabled = true;
+					}
+				}
+
+				image.fillAmount = progress;
+				image.color = new Color(1f, 1f, 1f, 0.3f + progress * 0.7f);
+
+				if(slot.Skill.CoolDown >= 1f) {
+					coolDownLabel.text = Mathf.CeilToInt(slot.Skill.CoolDown * (1f - progress)).ToString();
+					coolDownLabel.alpha = 0.1f + progress * 0.4f;
+				}
+			}
 		}
 	}
 }
